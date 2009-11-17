8f64b95f8d117557e2603514b9c0f85c5deda0f2
Pedro Mateus <pmtl3000@gmail.com>
2009-11-17 08:36:03 -0300

    Task C complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,13 +3,28 @@ class StoreController < ApplicationController
     @products = Product.find_products_for_sale
   end
 
-  def add_to_cart
-    @cart = find_cart
+  def add_to_cart  
     product = Product.find(params[:id])
-    @cart.add_product(product)
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid Product")
+    else
+      @cart = find_cart
+      @cart.add_product(product)
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index('Your cart is currently empty')
   end
 
   private
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action=>:index
+  end
+
   def find_cart
     session[:cart] ||= Cart.new
   end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,10 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-#ActionController::Base.session = {
-#  :key         => '_tutorial_rails_session',
-#  :secret      => 'd80ff244787044923b74d7963dee13b157cded2bec7398fb446f18c6ab6e4203e814aef08ed3e77655475233350e3469e19b7213e5ff5b3f4d61accd707473d8'
-#}
+ActionController::Base.session = {
+  :key         => '_tutorial_rails_session',
+  :secret      => 'd80ff244787044923b74d7963dee13b157cded2bec7398fb446f18c6ab6e4203e814aef08ed3e77655475233350e3469e19b7213e5ff5b3f4d61accd707473d8'
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

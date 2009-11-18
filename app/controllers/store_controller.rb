7b3b6597a355b441ed4dce72220c8dfbf84915a3
Pedro Mateus <pmtl3000@gmail.com>
2009-11-18 11:44:04 -0300

    Task D complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart  
@@ -10,17 +11,21 @@ class StoreController < ApplicationController
       redirect_to_index("Invalid Product")
     else
       @cart = find_cart
-      @cart.add_product(product)
+      @current_item = @cart.add_product(product)
+      respond_to do |format|
+        format.js if request.xhr?
+        format.html {redirect_to_index}
+      end
   end
 
   def empty_cart
     session[:cart] = nil
-    redirect_to_index('Your cart is currently empty')
+    redirect_to_index
   end
 
   private
 
-  def redirect_to_index(msg)
+  def redirect_to_index(msg = nil)
     flash[:notice] = msg
     redirect_to :action=>:index
   end

564d6cf9a5d8aaf01cbbc0de51a35d3f425a2cbe
Pedro Mateus <pmtl3000@gmail.com>
2009-11-30 08:02:59 -0300

    Task T Complete

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,61 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  fixtures :products
+
+  def test_invalid_with_empty_attributes
+    product = Product.new
+    assert !product.valid?
+    assert product.errors.invalid?(:title)
+    assert product.errors.invalid?(:description)
+    assert product.errors.invalid?(:price)
+    assert product.errors.invalid?(:image_url)
   end
-end
+
+  def test_positive_price
+    product = Product.new(:title        =>"Title",
+                          :description  =>"Description",
+                          :image_url        =>"wut.jpg")
+    product.price = -1
+    assert !product.valid?
+    assert_equal "should be at least 0.01", product.errors.on(:price)
+
+    product.price = 0
+    assert !product.valid?
+    assert_equal "should be at least 0.01", product.errors.on(:price)
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def test_image_url
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      product = Product.new(:title        => "My Book Title",
+                            :description  => "aaa",
+                            :price        => 1,
+                            :image_url    => name)
+    assert product.valid?, product.errors.full_messages
+    end
+
+    bad.each do |name|
+      product=Product.new(:title        => "MyBookTitle",
+                          :description  => "yyy",
+                          :price        => 1,
+                          :image_url    =>name)
+      assert !product.valid?, "saving#{name}"
+    end
+  end
+
+  def test_unique_title
+    product=Product.new(:title=>products(:ruby_book).title,
+    :description=> "yyy",
+    :price=>1,
+    :image_url=> "fred.gif")
+    assert !product.save
+    assert_equal ActiveRecord::Errors.default_error_messages[:taken], product.errors.on(:title)
+   end
+ end
